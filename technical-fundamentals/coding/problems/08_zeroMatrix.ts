278b566748dd4b56b65e832b94b68831728b4e01
Nahuel Luca <nahuelluca@MacBook-Pro-de-Nahuel.local>
2025-03-08 13:06:52 -0300

    resolve 07 and 08 problems

--- technical-fundamentals/coding/problems/08_zeroMatrix.ts
+++ technical-fundamentals/coding/problems/08_zeroMatrix.ts
@@ -2,8 +2,37 @@
 
 // Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
 
-type Matrix = number[][]
+// [1, 2]
+// [0, 4]
+// [5, 6]
+//
+// [0, 0]
+// [0, 0]
+// [0, 6]
 
-export default function zeroMatrix (matrix: Matrix) {
+type Matrix = number[][];
 
+export default function zeroMatrix(matrix: Matrix) {
+  const array: Array<[number, number]> = [];
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix.length; col++) {
+      const num = matrix[row][col];
+      if (num === 0) {
+        array.push([row, col]);
+      }
+    }
+  }
+
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  array.forEach((coord) => {
+    const [x, y] = coord;
+    matrix[x].fill(0);
+
+    for (let row = 0; row < matrix[y].length; row++) {
+      matrix[row][y] = 0;
+    }
+  });
+
+  return matrix;
 }
